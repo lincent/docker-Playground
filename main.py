47c7bfbbe18f5ec261a49bb9ba02a57dc579a052
Luke Vincent <11511477+lincent@users.noreply.github.com>
2024-06-06 13:30:21 +0100

    print some env variables pelase

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("I'm running over here on a docker container")
+import os
+from dotenv import load_dotenv
+
+
+def main():
+    load_dotenv()
+
+    mongo_uri = os.environ.get("MONGO_DB_URI")
+    mongo_db_name = os.environ.get("MONGO_DB_NAME")
+    openaq = os.environ.get("OPEN_AQ_API_KEY")
+    cds_uri = os.environ.get("CDSAPI_URL")
+    cds_key = os.environ.get("CDSAPI_KEY")
+
+    print("I'm running over here on a docker container")
+    print(f"mongo_uri = {mongo_uri}")
+    print(f"mongo_db_name = {mongo_db_name}")
+    print(f"openaq = {openaq}")
+    print(f"cds_uri = {cds_uri}")
+    print(f"cds_key = {cds_key}")
+
+
+if __name__ == "__main__":
+    main()
